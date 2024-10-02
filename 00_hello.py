9e58b918d3bb3d715ebad4bad9e1bb310217d3f0
ChissgaN <coderchissgan@gmail.com>
2024-10-02 00:10:11 -0600

    First commit in learning Python

---
+++ 00_hello.py
@@ -0,0 +1,15 @@
+#Este es mi primer comentario en Python en un Hola mundo;
+print ("Hola, Mundo!");    
+print ('Hola, Python!');
+
+print(type("Hola, Python"))   #Consultar el tipo de dato = str
+print(type(5))   #Consultar el tipo de dato = int
+print(type(1 + 7j))   #Consultar el tipo de dato = complex
+print(type(1.5))   #Consultar el tipo de dato = ftl
+
+"""
+Este es un
+comentario
+en varias 
+lineas
+"""
---
+++ 01_variables.py
@@ -0,0 +1,17 @@
+# Variables 
+# MyVariable = 'My String variable' aunque esto funciona la correcto es usar minusculas o snake case
+mi_variable = 'My String variable'
+print(mi_variable)
+
+my_int_variable = 5
+print(my_int_variable)
+
+my_int_to_str_variable = str(my_int_variable)
+print(my_int_to_str_variable)
+print(type(my_int_to_str_variable))
+
+my_bool_variable = False
+print(my_bool_variable)
+
+#concatenacion de variables en un print
+print(mi_variable, my_int_to_str_variable, my_bool_variable)
